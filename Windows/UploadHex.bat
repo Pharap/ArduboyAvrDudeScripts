a4ae1c8c46a12442631e1c22485be82cd1d93c41
Pharap <pharap@hotmail.com>
2017-09-24 19:15:04 +0100

    Update UploadHex.bat

--- Windows/UploadHex.bat
+++ Windows/UploadHex.bat
@@ -1,24 +1,39 @@
-@IF [%1] == [] (
-	@GOTO :usage
-)
-
-@IF [%2] == [] (
-	@GOTO :usage
-)
-
-@SETLOCAL
 @IF EXIST Config.bat (
 	@CALL Config.bat
-	@IF EXIST %1 (
-		@ECHO Uploading on %1
-		"%avrdudepath%" -v -patmega32u4 -cavr109 -P%1 -b57600 -D -Uflash:w:"%2":i
-	)
 ) ELSE (
 	@ECHO Config.bat not found
 	@ECHO Please call Setup.bat and provide it with the path to your avrdude.exe
+	@GOTO :end
 )
-@ENDLOCAL
-@GOTO :eof
 
-:usage
-@ECHO Usage: UploadHex COM# file.hex
+@SETLOCAL
+
+@IF [%1] == [] (
+	@SET /p comport=COM port: 
+) ELSE (
+	@SET comport=%1
+)
+
+@IF [%2] == [] (
+	@SET /p hexfile=.hex file: 
+) ELSE (
+	@SET hexfile=%2
+)
+
+@IF NOT EXIST %comport% (
+	@ECHO Invalid COM port
+	@GOTO :end
+)
+
+@IF NOT EXIST %hexfile% (
+	@ECHO Hex file not found
+	@GOTO :end
+)
+
+@ECHO Uploading %hexfile% on %comport%
+@%avrdudepath% -v -patmega32u4 -cavr109 -P%comport% -b57600 -D -Uflash:w:"%hexfile%":i
+
+@ENDLOCAL
+
+:end
+@SET "avrdudepath="
